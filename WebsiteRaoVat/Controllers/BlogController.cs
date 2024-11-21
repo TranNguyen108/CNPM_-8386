bebe7d8df40c869f4c4840987f0281f23933c477
Lecongphi <philepy2005@gmail.com>
2024-11-21 22:59:27 +0700

    Phi

---
+++ WebsiteRaoVat/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using WebsiteRaoVat.Models;
+
+namespace WebsiteRaoVat.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
